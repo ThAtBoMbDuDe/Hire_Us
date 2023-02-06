0db4ca3fccf3d1e5b0c4bc2ab7aa25d1c1863795
Jacob <117200950+ThAtBoMbDuDe@users.noreply.github.com>
2023-02-06 15:11:31 -0500

    still making changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Hire_Us
+This is a first attempt at project planning and production in a team environment. Working on creating a seamless pipeline so all members of a team can mold their work into one directory or project.
--- react-hire-us/src/components/NewInquiry/NewInquiry.jsx
+++ react-hire-us/src/components/NewInquiry/NewInquiry.jsx
@@ -1,6 +1,6 @@
 const NewInquiry = () => {
 
-  const newStudentInfo = {};
+  const newCompanyInfo = {};
 
   const postStudent = () => {
     fetch("http://34.121.174.111:3002/api/students", {
@@ -9,18 +9,18 @@ const NewInquiry = () => {
         'Content-Type': 'application/json'
       }, 
       method: "POST",
-      body: JSON.stringify(newStudentInfo)
+      body: JSON.stringify(newCompanyInfo)
     })
   }
  
-  const getStudentData = (e) => {
-    newStudentInfo.name = e.currentTarget.parentNode[0].value
-    newStudentInfo.email = e.currentTarget.parentNode[1].value
+  const getCompanyData = (e) => {
+    newCompanyInfo.name = e.currentTarget.parentNode[0].value
+    newCompanyInfo.email = e.currentTarget.parentNode[1].value
     
   }
 
   const handleClick = (e) => {
-    getStudentData(e)
+    getCompanyData(e)
     postStudent()
   }
 
